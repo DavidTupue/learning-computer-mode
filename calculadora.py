c5889a9fcecc28b3d195febbc15f7bf7b8d0a1c6
David Tupue <davidtupue@gmail.com>
2025-02-17 11:27:31 -0500

    este archivo tiene la calculadora y la division

--- calculadora.py
+++ calculadora.py
@@ -4,6 +4,8 @@ num2=4
 suma=num1+num2
 resta=num1-num2
 multiplicacion=num1*num2
+division=num1/num2
 print("la suma de los dos numeros es: ",suma)
 print("la resta de los dos numeros es: ",resta)
 print("la multiplicacion de los dos numeros es: ",multiplicacion)
+print("la division de los dos numeros es: ",division)
